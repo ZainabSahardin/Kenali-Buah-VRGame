bfe14d600331846a4d4ab2162ad4dc33cc467b4f
Siti Zainab Sahardin <zainabsahardin@gmail.com>
2023-07-10 15:08:46 +0800

    Delete unnecessary files

--- CPC452/Assets/Scripts/Fruit.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-public class Fruit : MonoBehaviour
-{
-    public int ID;
-    public string name;
-    public string pronunciation;
-}
--- CPC452/Assets/Scripts/Item.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-public class Item : MonoBehaviour
-{
-    public int itemID;
-    public string name;
-    public string pronunciation;
-}
